691ad393aba3428f42c0f44e1e201837cfaea2a6
GrylledCheez <68041324+GrylledCheez@users.noreply.github.com>
2023-05-27 06:56:34 -1000

    Update build.sh
    added package.json to ignore in the build file
    put the ignored files in alphabetical order

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rm wintile@nowsci.com.zip
 glib-compile-schemas schemas/
-zip -r9 wintile@nowsci.com.zip . -x '.git*' demo.gif README.md 'node_modules/*' build.sh .eslintrc.yml
+zip -r9 wintile@nowsci.com.zip . -x '.git*' build.sh demo.gif .eslintrc.yml 'node_modules/*' .package.json README.md 
