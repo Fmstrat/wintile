4f214690f342f4dfd6a6f7920c1efd7acbfbabbe
GrylledCheez <68041324+GrylledCheez@users.noreply.github.com>
2023-06-30 22:16:12 -1000

    update build.sh
    only include required files

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rm wintile@nowsci.com.zip
 glib-compile-schemas schemas/
-zip -r9 wintile@nowsci.com.zip . -x '.git*' build.sh demo.gif .eslintrc.yml 'node_modules/*' package.json README.md 
+zip -r9 wintile@nowsci.com.zip extension.js keybindings.js metadata.json prefs.js schemas
