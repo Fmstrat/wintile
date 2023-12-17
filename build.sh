fce6af36e7bb1842e4c9737a04d4924b785139a6
scar <84376771+ff2400t@users.noreply.github.com>
2023-12-17 16:39:22 +0530

    adding settings.ui to the build.sh packaging

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 rm wintile@nowsci.com.zip
 glib-compile-schemas schemas/
-zip -r9 wintile@nowsci.com.zip extension.js keybindings.js metadata.json prefs.js schemas
+zip -r9 wintile@nowsci.com.zip extension.js keybindings.js settings.ui metadata.json prefs.js schemas
